0a9b37b717ed52a5f69c43eb44190eb7c9685bf6
pantifik <pantifik13@gmail.com>
2018-05-24 01:23:03 +0300

    кроха коментов

--- index.js
+++ index.js
@@ -13,19 +13,25 @@ module.exports = {
                "functions": false 
             }            
         ],
+        // разрешаем использовать console.
         "no-console": 0,
+        // большие отсупы
         "no-multi-spaces": [
             "error",
             {
-                "ignoreEOLComments": true,
+                "ignoreEOLComments": true,          // в коментариях
                 "exceptions": { 
-                    "VariableDeclarator": true }
+                    "VariableDeclarator": true }    // в объявление переменных
             }
         ],
+        // совместное объявление переменных
         "one-var": [
             "error",
             "always"
         ],
+        // каждая переменная на новой строке
+        // let a,
+        //     b = 10;
         "one-var-declaration-per-line": [
             "error", 
             "always"
